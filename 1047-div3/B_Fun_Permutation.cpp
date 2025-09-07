d8c2084d8d0a8cb69521eb6f7d44b698c213fea4
Kaushal Singh <80886710+17kaushalsingh@users.noreply.github.com>
2025-09-07 22:58:12 +0530

    Sep 7 2025

---
+++ 1047-div3/B_Fun_Permutation.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> f(vector<int> &arr, int n)
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+        for (int i=0; i<n; i++) cin >> arr[i];
+
+        vector<int> ans = f(arr, n);
+        for (int &x : ans) cout << x << " ";
+        cout << endl;
+
+    }
+
+    return 0;
+}
